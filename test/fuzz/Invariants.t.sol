2cbb1153546ceb35c077ee305ad3ba239ddc94d4
LordDregg <oluwatosinodejide121@gmail.com>
2026-02-10 00:35:55 +0100

    course finish

--- test/fuzz/Invariants.t.sol
+++ test/fuzz/Invariants.t.sol
@@ -1,15 +1,47 @@
-// SPDX-Licence-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.3;
+pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {DeployDSC} from "script/DeployDSC.s.sol";
 import {DecentralizedStableCoin} from "src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "src/DSCEngine.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "test/fuzz/Handler.t.sol";
 
-contract Invariant is Test {
-  
+contract InvariantsTest is StdInvariant, Test {
+  DeployDSC deployer;
+  DecentralizedStableCoin dsc;
+  DSCEngine engine;
+  HelperConfig config;
+  address weth;
+  address wbtc;
+  Handler handler;
+
+  function setUp() external {
+    deployer = new DeployDSC();
+    (dsc, engine, config) = deployer.run();
+    (,, weth, wbtc,) = config.activeNetworkConfig();
+    // targetContract(address(engine));
+    handler = new Handler(dsc, engine);
+    targetContract(address(handler));
+  }
+
+  function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+    uint256 totalSupply = dsc.totalSupply();
+    uint256 totalWethDeposited = IERC20(weth).balanceOf(address(engine));
+    uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(engine));
+
+    uint256 wethValue = engine.getUsdValue(weth, totalWethDeposited);
+    uint256 wbtcValue = engine.getUsdValue(wbtc, totalWbtcDeposited);
+
+    assert(wethValue + wbtcValue >= totalSupply);
+  }
+
+  function invariant_gettersShouldNotRevert() public view {
+    engine.getLiquidationBonus();
+    engine.getPrecision();
+  }
 }
